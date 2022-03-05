d273d8e01e9cb10ffdf852830fee1fc6937b954d
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-05 15:47:53 +0100

    Improving comments

--- front/js/cart.js
+++ front/js/cart.js
@@ -221,7 +221,7 @@ form.addEventListener("submit", (event) => {
   fetch("http://localhost:3000/api/products/order", {
     method: "POST",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ contact, products }),
